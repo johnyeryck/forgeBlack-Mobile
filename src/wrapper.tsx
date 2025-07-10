656fbfdf39cf9d9b560db3b3f58b530105ee5d5b
johnyeryck <johnybombado@gmail.com>
2025-07-10 17:11:07 -0300

    feat: Implement Wrapper component with data fetching and context provider

---
+++ src/wrapper.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { MyContext } from "./context/contextApp";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/router";
+
+function Wrapper(){
+    const { data } = useQuery({
+    queryKey: ["produtos"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3000/produtos", {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      return response.json();
+    },
+  });
+    return(
+        <MyContext value={data}>
+            <RouterProvider router={router}/>
+        </MyContext>
+    )
+}
+
+export default Wrapper
