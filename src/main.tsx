5a163ddef468f50617e6010e147b8bd523f32234
johnyeryck <johnybombado@gmail.com>
2025-07-10 17:11:03 -0300

    refactor: Remove routing logic and unused components from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css'
-import Buypage from './pages/Buypage';
+import Wrapper from './wrapper';
 const queryClient = new QueryClient();
-const router = createBrowserRouter([
-  {
-    path : "/produto/:slug",
-    element : <Buypage />
-  },
-  {
-    path : "/",
-    element : <App />
-  }
-])
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}/>
+        <Wrapper />
     </QueryClientProvider>
   </React.StrictMode>
 );
