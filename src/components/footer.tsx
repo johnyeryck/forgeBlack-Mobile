8885afaa97f56ccc1802d520ebdcb5e5cbce24d0
johnyeryck <johnybombado@gmail.com>
2025-07-09 21:39:08 -0300

    feat: Rearrange footer links and images for better layout in Footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,13 +6,6 @@ function Footer (){
             <div className="size-35">
                 <h3 className="text-2xl">Black Forge</h3>
                 <p className="font-light">Suplementos e Performance com sua identidade.</p>
-                 <div className='flex'>
-                     <img src={Instagram} alt="" className='size-7 mt-4 ' />
-                     <a href="https://api.whatsapp.com/send/?phone=83999246257&text&type=phone_number&app_absent=0">
-                     <img src={Whats} alt="" className='size-7 mt-4 ml-2' />
-                     </a>
-                     
-                 </div>
             </div>
             <article className="">
                 <p className="text-2xl">Links</p>
@@ -20,6 +13,13 @@ function Footer (){
                     <a href="">Contato</a>
                     <a href="">forgeblack6@gmail.com</a>
                     <a href="">Política de Privacidade</a>
+                 <div className='flex'>
+                     <img src={Instagram} alt="" className='size-7 mt-4 ' />
+                     <a href="https://api.whatsapp.com/send/?phone=83999246257&text&type=phone_number&app_absent=0">
+                     <img src={Whats} alt="" className='size-7 mt-4 ml-2' />
+                     </a>
+                     
+                 </div>
                 </div>
 
             </article>
--- src/pages/Buypage.tsx
+++ src/pages/Buypage.tsx
@@ -1,6 +1,26 @@
+import { useContext } from "react";
+import Navbar from "../components/Navbar"
+import { useParams } from "react-router-dom"
+import { MyContext } from "../context/contextApp";
 function Buypage(){
+    const { slug } = useParams();
+    const slugcomp = slug?.replace(/-/g,"")
+    const data = useContext(MyContext)
     return(
-        <header></header>
+        <header>
+            <Navbar />
+            {
+               data && data.length > 0 ? (
+                    data.map((P)=>(
+                        P.nome === slugcomp ? (
+                            <div className="border">
+                                <img src={P.img} alt="" />
+                            </div>
+                        ): null
+                    ))
+               ): null
+            }
+        </header>
     )
 }
 
