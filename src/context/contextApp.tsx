dddb49ecf56581ef2c62bb5e331e469a79bbd82f
johnyeryck <johnybombado@gmail.com>
2025-07-11 12:47:07 -0300

    feat: Add description field to produtos interface in contextApp

--- src/context/contextApp.tsx
+++ src/context/contextApp.tsx
@@ -5,6 +5,7 @@ interface produtos {
     valor : number
     img: string,
     value_anterior : string
+    description : string
 }
 
 export const MyContext = createContext<produtos[] | undefined>(undefined)
