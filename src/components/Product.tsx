bfa95045ffa280fdc0f1df82471db5a757d1f295
johnyeryck <johnybombado@gmail.com>
2025-07-11 12:47:01 -0300

    feat: Move slugify function into Product component and enhance product name display

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom"
-import slugify from "slugify"
 interface Productinterface {
     id : number,
     valor : number,
@@ -8,14 +7,27 @@ interface Productinterface {
     value_anterior?: string
 }
 
-const Product : React.FC<Productinterface> = ({img , nome , valor , value_anterior})=>{
+
+function slugify(texto : string) {
+  return texto
+    .toLowerCase()
+    .normalize('NFD')                   // separa acentos das letras
+    .replace(/[\u0300-\u036f]/g, '')    // remove os acentos
+    .replace(/[^\w\s-]/g, '')           // remove caracteres especiais
+    .trim()
+    .replace(/\s+/g, '-')               // troca espaços por hífens
+    .replace(/-+/g, '-');               // evita hífens duplicados
+}
+
+
+const Product : React.FC<Productinterface> = ({img , nome , valor , value_anterior,id})=>{
     return(
-        <article className="border-b rounded-lg h-auto w-41">
-            <Link to={`/produto/${slugify(nome)}`}>
+        <article className="border-b rounded-lg h-auto w-41" key={id}>
+            <Link to={`/produto/${slugify(nome)}/${id}`}>
             <img src={img} alt={`Imagem de ${nome}`} className="rounded-xl ml-auto mr-auto" />
-            <div className="rounded-lg ">
+            <div className="rounded-lg">
                 <div className="h-18">
-                    <h3 className="font-light pl-1 text-white">{nome}</h3>
+                        <h3 className="font-light pl-1 text-white ">{nome.length > 25 ? nome.slice(0,20) + "..." : nome}</h3>
                 </div>
                 <div className="flex pt-2 ">
                     <p className="text-green-400 font-bold pl-2">R${valor}</p>
