7fdad080217ab2ce637b1b62d7b9a6e616f49f82
johnyeryck <johnybombado@gmail.com>
2025-07-11 23:39:38 -0300

    fix: Adjust spacing and improve WhatsApp link message in Buypage component

--- src/pages/Buypage.tsx
+++ src/pages/Buypage.tsx
@@ -16,17 +16,17 @@ function Buypage(){
                 data && data.length > 0 ? (
                     data.map((P)=>(
                         P.id === Number(id) ? (
-                            <section className=" mt-20 mb-20" key={P.id}>
+                            <section className=" mt-25 mb-20" key={P.id}>
                                 <Link to={"/"}>
-                                <GoArrowLeft className="size-12 mt-10 rounded-full  bg-green-500 ml-7 mb-10" />
+                                <GoArrowLeft className="size-12 mt-10 rounded-full ml-10 mb-4 bg-green-500 " />
                                 </Link>
-                                <img src={P.img} alt="" className="ml-auto mr-auto rounded-lg size-[80%]"/>
+                                <img src={P.img} alt="" className="ml-auto mr-auto rounded-lg size-[80%] "/>
                                 <div className=" w-[80%] ml-auto mr-auto mt-2">
                                     <p className="text-green-500  text-2xl ">R${P.valor}</p>
                                     <p className="text-white ">{P.nome}</p>
                                 </div>
                                 <div className="w-[80%] ml-auto mr-auto h-10 mb-10 mt-2 ">
-                                    <a href={`whatsapp://send?phone=5583999246257&text=${encodeURIComponent(`Olá Gostaria de saber mais sobre ${P.nome}`)}`}
+                                    <a href={`whatsapp://send?phone=5583999246257&text=${encodeURIComponent(`Olá, Gostaria de saber mais sobre o(a)${P.nome}`)}`}
                                     className="w-full  h-full block"
                                     role="button"
                                     aria-label="Fale conosco pelo whatsapp"
