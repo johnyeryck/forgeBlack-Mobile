15eccee5a75be17b72016c4cd984427054219c97
johnyeryck <johnybombado@gmail.com>
2025-07-11 23:39:31 -0300

    feat: Implement lazy loading for App and Buypage components in router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,15 +1,26 @@
-import { createBrowserRouter } from "react-router-dom"
-import App from "../App"
-import Buypage from "../pages/Buypage"
+import  { Suspense, lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+const App = lazy(() => import("../App"));
+const Buypage = lazy(() => import("../pages/Buypage"));
 
 const router = createBrowserRouter([
   {
-    path : "/produto/:slug/:id",
-    element : <Buypage />
+    path: "/produto/:slug/:id",
+    element: (
+      <Suspense fallback={<div>Carregando...</div>}>
+        <Buypage />
+      </Suspense>
+    )
   },
   {
-    path : "/",
-    element : <App />
+    path: "/",
+    element: (
+      <Suspense fallback={<div>Carregando...</div>}>
+        <App />
+      </Suspense>
+    )
   }
-])
-export default router
+]);
+
+export default router;
