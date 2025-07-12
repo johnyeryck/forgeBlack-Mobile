902099fd30c8e9d4a3e85a055cb92354024d6d95
johnyeryck <johnybombado@gmail.com>
2025-07-12 00:36:46 -0300

    error resolved

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 const app = express()
 app.use(
   cors({
-    origin: ["https://www.blackforge.com.br/"
+    origin: ["https://www.blackforge.com.br"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
